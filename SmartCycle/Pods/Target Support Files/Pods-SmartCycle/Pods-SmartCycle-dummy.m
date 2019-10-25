7a759876c335e95efa574e67215e664db9272ca3
Tam Nguyen <tamnguyen@dhcp-10-8-028-198.mobile.reshsg.uci.edu>
2019-10-25 01:52:20 -0700

    Started project

---
+++ SmartCycle/Pods/Target Support Files/Pods-SmartCycle/Pods-SmartCycle-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SmartCycle : NSObject
+@end
+@implementation PodsDummy_Pods_SmartCycle
+@end
---
+++ SmartCycle/Pods/Target Support Files/Pods-SmartCycleTests/Pods-SmartCycleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SmartCycleTests : NSObject
+@end
+@implementation PodsDummy_Pods_SmartCycleTests
+@end
