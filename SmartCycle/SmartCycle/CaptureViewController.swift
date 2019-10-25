4c6d11148589590dc23fce4ba0c758a99f56b249
Tam Nguyen <tamnguyen@dhcp-10-8-028-198.mobile.reshsg.uci.edu>
2019-10-25 02:31:59 -0700

    Added camera button

---
+++ SmartCycle/SmartCycle/CaptureViewController.swift
@@ -0,0 +1,60 @@
+//
+//  CaptureViewController.swift
+//  SmartCycle
+//
+//  Created by Tam Nguyen on 10/25/19.
+//  Copyright © 2019 Rho Bros Co. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class CaptureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func capture(_ sender: Any) {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageAspectScaled(toFill: size)
+        
+        imageView.image = scaledImage
+        dismiss(animated: true, completion: nil)
+        
+        
+    }
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
